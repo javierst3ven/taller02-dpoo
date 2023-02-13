39001be57570575fdf24d491093d537707262326
javi3r <js.barrerat1@uniandes.edu.co>
2023-02-13 16:23:26 -0500

    first commit

---
+++ src/uniandes/taller2/consola/Consola.java
@@ -0,0 +1,8 @@
+package uniandes.taller2.consola;
+import uniandes.taller2.modelo.Hamburguesa;
+
+public class Consola {
+	public static void main(String[] args) {
+		System.out.println("a");
+	}
+}
---
+++ src/uniandes/taller2/modelo/Hamburguesa.java
@@ -0,0 +1,5 @@
+package uniandes.taller2.modelo;
+
+public class Hamburguesa {
+
+}

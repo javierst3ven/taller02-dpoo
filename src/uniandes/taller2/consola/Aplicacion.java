2d1405776f5d0630be6852f7b9a4dd96bd002379
javi3r <js.barrerat1@uniandes.edu.co>
2023-02-21 23:55:24 -0500

    remove: unused imports

--- src/uniandes/taller2/consola/Aplicacion.java
+++ src/uniandes/taller2/consola/Aplicacion.java
@@ -1,12 +1,10 @@
 package uniandes.taller2.consola;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import uniandes.taller2.modelo.Restaurante;
 import uniandes.taller2.modelo.Pedido;
